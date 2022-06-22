3288e051e78f83c58dd61c538c06cc7ab431b2d7
Philipp Chapkovski <chapkovski@gmail.com>
2022-06-22 20:18:10 +0200

    small upd of messagesin in awards desc

--- src/awards.js
+++ src/awards.js
@@ -11,22 +11,26 @@ const awards = [
     lock: true,
     locked: require("@/assets/silver_locked.png"),
     unlocked: require("@/assets/silver.png"),
+    message: "You belong on the trading floor! 👍",
   },
   {
     lock: true,
     locked: require("@/assets/gold_locked.png"),
     unlocked: require("@/assets/gold.png"),
+    message: "You are the money maker! 👍",
   },
 
   {
     lock: true,
     locked: require("@/assets/platinum_locked.png"),
     unlocked: require("@/assets/platinum.png"),
+    message: "You are definitely going places! 👍",
   },
   {
     lock: true,
     locked: require("@/assets/diamond_locked.png"),
     unlocked: require("@/assets/diamond.png"),
+    message: "The Wolf of Wall Street  👍",
   },
 ];
 export default awards;
