b8fb057f068d0829b44752f776e0d08414b55ac9
Philipp Chapkovski <chapkovski@gmail.com>
2022-06-23 18:01:49 +0200

    upd gold

--- src/awards.js
+++ src/awards.js
@@ -4,33 +4,37 @@ const awards = [
     locked: require("@/assets/bronze_locked.png"),
     unlocked: require("@/assets/bronze.png"),
     message: "Level up! Doing well 👍",
-    url: "https://bit.ly/3xtGrWk",
+    gif: require("@/assets/gold.gif"),
   },
 
   {
     lock: true,
     locked: require("@/assets/silver_locked.png"),
     unlocked: require("@/assets/silver.png"),
-    message: "You belong on the trading floor! 👍",
+    message: "You belong on the trading floor! 📈",
+    gif: require("@/assets/gold.gif"),
   },
   {
     lock: true,
     locked: require("@/assets/gold_locked.png"),
     unlocked: require("@/assets/gold.png"),
-    message: "You are the money maker! 👍",
+    message: "You are the money maker! 💰",
+    gif: require("@/assets/gold.gif"),
   },
 
   {
     lock: true,
     locked: require("@/assets/platinum_locked.png"),
     unlocked: require("@/assets/platinum.png"),
-    message: "You are definitely going places! 👍",
+    message: "You are definitely going places! 👐",
+    gif: require("@/assets/gold.gif"),
   },
   {
     lock: true,
     locked: require("@/assets/diamond_locked.png"),
     unlocked: require("@/assets/diamond.png"),
-    message: "The Wolf of Wall Street  👍",
+    message: "The Wolf of Wall Street  ☝️",
+    gif: require("@/assets/gold.gif"),
   },
 ];
 export default awards;
--- src/components/AwardGiven.vue
+++ src/components/AwardGiven.vue
@@ -3,11 +3,7 @@
     <div class="d-flex flex-column justify-center align-center">
       <v-card elevation="3" width="600">
         <v-card-title class="d-flex align-center justify-center text-center">
-          <video width="320" autoplay loop>
-            <source src="@/assets/bronze.mp4" type="video/mp4" />
-
-            Your browser does not support the video tag.
-          </video>
+          <v-img contain :src="awardGiven.gif" class="mx-3"></v-img>
         </v-card-title>
         <v-card-text>
           <h3 class="text-center">{{ awardGiven.message }}</h3>
